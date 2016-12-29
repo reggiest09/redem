be9845a82867bbe107d18dfdb57f87d86ac0a9be
Reggie <reggiest09@gmail.com>
2016-12-29 01:10:37 -0500

    font

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,8 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
+  config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
