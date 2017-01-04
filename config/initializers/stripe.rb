3b1d700967a8a981d34cd5ece024fba4e51416a9
Reggie <reggiest09@gmail.com>
2017-01-04 14:44:21 -0500

    stp

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,16 +1,1 @@
- Rails.configuration.stripe = {
-   :publishable_key => 'sk_test_YMVVOieBFHbvZA40m7ziQbi9',
-   :secret_key      => 'pk_test_ArNqXJH1zSOAOOT18XoME2pF'
- }
-
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
-
-
-
-
-# Rails.configuration.stripe = {
-#   :publishable_key => ENV['PUBLISHABLE_KEY'],
-#   :secret_key      => ENV['SECRET_KEY']
-# }
-
-# Stripe.api_key = Rails.configuration.stripe[:secret_key]
+Stripe.api_key = ENV['STRIPE_SECRET_KEY']
