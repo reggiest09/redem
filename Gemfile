6ec7aa6642341da43f680cd0beb460ef5a6b6d69
Reggie <reggiest09@gmail.com>
2017-01-04 11:42:44 -0500

    aws3

--- Gemfile
+++ Gemfile
@@ -40,8 +40,7 @@ gem "figaro"
   gem 'active_skin'
 
 gem "paperclip", "~> 5.0.0"
-gem 'aws-sdk', '< 2.0'
-
+gem 'aws-sdk', '~> 2.3'
 
 
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,10 +86,14 @@ config.paperclip_defaults = {
   s3_credentials: {
     bucket: ENV.fetch('S3_BUCKET_NAME'),
     access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY')
-    
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    s3_region: ENV.fetch('AWS_REGION'),
   }
 }
+
+
+
+
 
 
 
